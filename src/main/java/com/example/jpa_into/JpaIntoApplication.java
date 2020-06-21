6a185d087a1953b988c4d396d90300bcaeb7567a
MikiKru <michal_kruczkowski@o2.pl>
2020-06-21 09:32:45 +0200

    jpa -d1 -v0

---
+++ src/main/java/com/example/jpa_into/JpaIntoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_into;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaIntoApplication.class, args);
+    }
+
+}
